bae004751fb5ff3b4d97506627500fab1d8b915f
Aviral Singh Chauhan <59026666+sAVItar02@users.noreply.github.com>
2022-01-29 01:59:23 +0530

    feat: Dream Sucker added random movement

--- Assets/_Scripts/Enemy Scripts/DreamSucker.cs
+++ Assets/_Scripts/Enemy Scripts/DreamSucker.cs
@@ -6,9 +6,17 @@ public class DreamSucker : MonoBehaviour
 {
     private SpriteRenderer sprite;
     private Rigidbody2D rb;
+
+    [SerializeField] float minX;
+    [SerializeField] float maxX;
+    [SerializeField] float minY;
+    [SerializeField] float maxY;
+    Vector2 targetPosition;
+    [SerializeField] float speed;
     private void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
+        targetPosition = GetRandomPosition();
     }
     void Update()
     {
@@ -19,5 +27,21 @@ public class DreamSucker : MonoBehaviour
         {
             sprite.flipX = true;
         }
+
+        if ((Vector2)transform.position != targetPosition)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        }
+        else
+        {
+            targetPosition = GetRandomPosition();
+        }
+    }
+
+    Vector2 GetRandomPosition()
+    {
+        float randomX = Random.Range(minX, maxX);
+        float randomY = Random.Range(minY, maxY);
+        return new Vector2(randomX, randomY);
     }
 }
